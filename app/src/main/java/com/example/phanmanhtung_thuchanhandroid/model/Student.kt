654f04c528db267400c2665dde5f4e260b3490ef
PhanManhTung-347 <phanmanhtung24112006@gmail.com>
2026-09-18 07:09:18 +0700

    Tao Data Model & Extendsion Functions

---
+++ app/src/main/java/com/example/phanmanhtung_thuchanhandroid/model/Student.kt
@@ -0,0 +1,9 @@
+package com.example.phanmanhtung_thuchanhandroid.model
+
+data class Student(
+    val id: String,
+    val name: String,
+    val className: String,
+    val email: String,
+    val gpa: Double
+)
---
+++ app/src/main/java/com/example/phanmanhtung_thuchanhandroid/utils/ViewExtensions.kt
@@ -0,0 +1,21 @@
+package com.example.phanmanhtung_thuchanhandroid.utils
+
+import android.content.Context
+import android.widget.Toast
+
+fun Double.toAcademicRanking(): String {
+    return when {
+        this >= 3.6 -> "Xuất sắc!!"
+        this >= 3.2 -> "Giỏi"
+        this >= 2.5 -> "Khá"
+        else -> "Trung bình"
+    }
+}
+
+fun Context.toast(message: String) {
+    Toast.makeText(
+        this,
+        message,
+        Toast.LENGTH_SHORT
+    ).show()
+}
